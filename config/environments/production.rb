8e81e4ac2cd398adb7896f54060f050045e1454a
Morgan Davison <morgan.davison@gmail.com>
2015-08-21 21:00:50 -0700

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   host = 'https://radiant-scrubland-5691.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.mandrillapp.com'
+    :address              => 'smtp.mandrillapp.com',
     :port                 => '587',
     :authentication       => :plain,
     :user_name            => ENV['MANDRILL_USERNAME'],
